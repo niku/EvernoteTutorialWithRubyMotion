a75fb98892ba341ee39c763b44e8dbb334dfc0cd
niku <niku@niku.name>
2014-02-27 00:23:01 +0900

    Configure for creating session of Evernote

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    evernote_host = BootstrapServerBaseURLStringSandbox
+    consumer_key = MY_ENV['CONSUMER_KEY']
+    consumer_secret = MY_ENV['CONSUMER_SECRET']
+
+    EvernoteSession.setSharedSessionHost(evernote_host, consumerKey: consumer_key, consumerSecret: consumer_secret)
+
     true
   end
 end
